9acc0bd183e049d8a5efa0ac5124d01dd19c00b7
Marcus Stojcevich <mstojcevich@gmail.com>
2017-09-16 21:51:46 -0400

    Remove dlangui stuff for now

--- source/app.d
+++ source/app.d
@@ -1,36 +1,36 @@
 import std.stdio;
 import gameboy;
-import dlangui;
 import std.parallelism;
 
-mixin APP_ENTRY_POINT;
+// mixin APP_ENTRY_POINT;
 
-extern (C) int UIAppMain(string[] args) {
-    // Set preferences for OpenGl context of the window
-    // Requesting 3.0 because we use glDrawPixels, which
-    //  is part of the fixed-function pipeline
-    Platform.instance.GLVersionMajor = 3;
-    Platform.instance.GLVersionMinor = 0;
+// extern (C) int UIAppMain(string[] args) {
+//     // Set preferences for OpenGl context of the window
+//     // Requesting 3.0 because we use glDrawPixels, which
+//     //  is part of the fixed-function pipeline
+//     Platform.instance.GLVersionMajor = 3;
+//     Platform.instance.GLVersionMinor = 0;
 
-    Window window = Platform.instance.createWindow("DDMG", null);
+//     Window window = Platform.instance.createWindow("DDMG", null);
 
-    window.mainWidget = (new Button()).text("Hello world!"d).margins(Rect(20, 20, 20, 20));
+//     window.mainWidget = (new Button()).text("Hello world!"d).margins(Rect(20, 20, 20, 20));
 
-    window.show();
+//     window.show();
 
-    auto t = task!startGB;
-    t.executeInNewThread();
+//     auto t = task!startGB;
+//     t.executeInNewThread();
 
-    return Platform.instance.enterMessageLoop();
-}
+//     return Platform.instance.enterMessageLoop();
+// }
 
-void startGB() {
+// void startGB() {
+//     Gameboy g = new Gameboy();
+//     g.run();
+// }
+
+void main() {
+    writeln("Starting emulator");
+
     Gameboy g = new Gameboy();
     g.run();
 }
-
-// void main() {
-//     writeln("Starting emulator");
-
-//     Gameboy g = new Gameboy();
-// }
