99d2a3a908a4b5b361a0dc03900b20d2dea48e86
Marcus Stojcevich <mstojcevich@gmail.com>
2017-09-29 11:21:18 -0400

    Remove unused GUI stuff

--- source/app.d
+++ source/app.d
@@ -3,32 +3,6 @@ import gameboy;
 import std.parallelism;
 import std.getopt;
 
-// mixin APP_ENTRY_POINT;
-
-// extern (C) int UIAppMain(string[] args) {
-//     // Set preferences for OpenGl context of the window
-//     // Requesting 3.0 because we use glDrawPixels, which
-//     //  is part of the fixed-function pipeline
-//     Platform.instance.GLVersionMajor = 3;
-//     Platform.instance.GLVersionMinor = 0;
-
-//     Window window = Platform.instance.createWindow("DDMG", null);
-
-//     window.mainWidget = (new Button()).text("Hello world!"d).margins(Rect(20, 20, 20, 20));
-
-//     window.show();
-
-//     auto t = task!startGB;
-//     t.executeInNewThread();
-
-//     return Platform.instance.enterMessageLoop();
-// }
-
-// void startGB() {
-//     Gameboy g = new Gameboy();
-//     g.run();
-// }
-
 void main(string[] args) {
     writeln("Starting emulator");
 
