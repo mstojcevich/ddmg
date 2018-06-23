4fe368d32e8d4ecc8c166d40d21dea86911949f0
Marcus Stojcevich <mstojcevich@duosecurity.com>
2018-06-23 03:27:58 -0400

    Add support for automatically running the rest of blargg's tests (aside from halt_bug)

--- source/app.d
+++ source/app.d
@@ -1,14 +1,16 @@
 import core.stdc.stdlib;
 import std.stdio;
 import gameboy;
+import mmu;
 import std.algorithm.searching;
+import std.ascii;
 import std.parallelism;
 import std.getopt;
 import frontend;
 import frontend.sdl;
 import frontend.test;
 
-private enum TestMode { none, blargg }
+private enum TestMode { none, blargg_serial, blargg_memory }
 
 string romName = "../opus5.gb";
 TestMode testMode = TestMode.none;
@@ -19,8 +21,8 @@ long maxInstrs = 25_000_000;  // Max instructions to run a test rom for
 
     readArgs(args);
 
-    if (testMode == TestMode.blargg) {
-        runBlarggTest();
+    if (testMode != TestMode.none) {
+        runTest();
     } else {
         Frontend frontend = new SDLFrontend();
         frontend.init();
@@ -30,7 +32,7 @@ long maxInstrs = 25_000_000;  // Max instructions to run a test rom for
     }
 }
 
-@trusted private void runBlarggTest() {
+@trusted private void runTest() {
     TestFrontend frontend = new TestFrontend();
     frontend.init();
 
@@ -40,11 +42,48 @@ long maxInstrs = 25_000_000;  // Max instructions to run a test rom for
     string serialOut = frontend.getSerialOutput().toString();
     writefln("FINAL SERIAL OUTPUT: %s", serialOut);
 
-    if(!canFind(serialOut, "Passed")) {
-        stderr.writeln("FAILED: Didn't pass all tests");
-        exit(1);
-    } else {
+    bool passed = false;
+    string failReason = "Unexpected failure";
+    if (testMode == TestMode.blargg_serial) {
+        passed = canFind(serialOut, "Passed");
+        if(!passed) {
+            failReason = "Didn't pass all tests";
+        }
+    } else if (testMode == TestMode.blargg_memory) {
+        const(MMU) m = g.getMMU();
+        ubyte testStatus = m.readByte(0xA000);
+
+        if (m.readByte(0xA001) != 0xDE || m.readByte(0xA002) != 0xB0 || m.readByte(0xA003) != 0x61) {
+            passed = false;
+            failReason = "Incorrect signature data. Ended too early?";
+        } else if (testStatus == 0x80) {
+            passed = false;
+            failReason = "Test was still running when output was checked.";
+        } else {
+            writefln("Final status code: %d", testStatus);
+
+            string statusString = "";
+            int addr = 0xA004;
+            ubyte curChar = m.readByte(addr);
+            while(curChar != 0 && isASCII(curChar)) {
+                statusString ~= cast(char)(curChar);
+                addr++;
+                curChar = m.readByte(addr);
+            }
+            writefln("Final status string: %s", statusString);
+
+            passed = testStatus == 0;
+            if(!passed) {
+                failReason = "Bad test status code.";
+            }
+        }
+    }
+    
+    if (passed) {
         writeln("PASSED!");
+    } else {
+        stderr.writefln("FAILED: %s", failReason);
+        exit(1);
     }
 }
 
--- source/gameboy.d
+++ source/gameboy.d
@@ -56,4 +56,8 @@ class Gameboy {
         }
     }
 
+    @safe public const(MMU) getMMU() const {
+        return this.mmu;
+    }
+
 }
